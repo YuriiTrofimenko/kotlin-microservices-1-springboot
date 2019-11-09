bd6a93ab965b046edc41ce397aae02d531522578
YuriiTrofimenko <tyaa@ukr.net>
2019-11-09 14:23:07 +0200

    value annotation

--- src/main/kotlin/org/tyaa/simplespringbootapp/FirstService.kt
+++ src/main/kotlin/org/tyaa/simplespringbootapp/FirstService.kt
@@ -1,8 +1,15 @@
 package org.tyaa.simplespringbootapp
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class FirstService {
-    fun getHello(name : String) = "hello $name"
+    @Value(value = "\${service.message.text}")
+    private lateinit var text: String
+
+    @Value(value = "\${service.message.suffix}")
+    private lateinit var suffix: String
+
+    fun getHello(name : String) = "$text $name$suffix"
 }
