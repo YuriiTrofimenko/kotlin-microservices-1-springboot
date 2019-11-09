bbe49413e7adf66d22575ca788fe94aad8e4aaf0
YuriiTrofimenko <tyaa@ukr.net>
2019-11-09 13:19:57 +0200

    first controller

--- src/main/kotlin/org/tyaa/simplespringbootapp/SimpleSpringbootAppApplication.kt
+++ src/main/kotlin/org/tyaa/simplespringbootapp/SimpleSpringbootAppApplication.kt
@@ -2,9 +2,20 @@ package org.tyaa.simplespringbootapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class SimpleSpringbootAppApplication
+
+@Controller
+class FirstController {
+	@RequestMapping(value = ["/user"], method = arrayOf(RequestMethod.GET))
+	@ResponseBody
+	fun hello() = "Hello Kotlin!"
+}
 
 fun main(args: Array<String>) {
 	runApplication<SimpleSpringbootAppApplication>(*args)
